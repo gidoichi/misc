9395aec2802f21ce33744b62ee3dae68f5afd5d4
gidoichi <shinichi@FJITSU-ESPRIMO.localdomain>
2019-11-16 12:17:00 +0900

    行列計算を使用

--- NeuralNetwork/NeuralNetwork.hpp
+++ NeuralNetwork/NeuralNetwork.hpp
@@ -5,6 +5,7 @@
 #include <random>
 #include <vector>
 #include <iostream>
+#include <Eigen/Dense>
 
 template <std::size_t N>
 class NeuralNetwork
@@ -16,16 +17,18 @@ private:
   // const std::size_t output_units_length_ = 1;
   // 中間層のユニット数
   const std::size_t middle_units_length_ = N;
-  // weights_[k][j][i]: 中間層(k-1)のi番目のニューロンと
+  // weights_[k](j, i): 中間層(k-1)のi番目のニューロンと
   // 中間層kのj番目のニューロンをつなぐ重み。
   // ただし、-1番目の層を入力層、1番目の層を出力層とする。
-  std::vector< std::vector< std::vector<double> > > weights_;
+  std::array<Eigen::MatrixXd, 2> weights_;
   // 学習率
   double learning_rate_ = 0.01;
   // 活性化関数
   double activation_function(double x);
+  Eigen::MatrixXd activation_function(Eigen::MatrixXd x);
   // 活性化関数の導関数
   double derivative_activation_function(double x);
+  Eigen::MatrixXd derivative_activation_function(Eigen::MatrixXd x);
 
 public:
   // 慣性項の寄与率
@@ -44,21 +47,16 @@ template<std::size_t N>
 NeuralNetwork<N>::NeuralNetwork()
 {
   // 必要な領域の確保
-  weights_.resize(2);
-  weights_[0].resize(middle_units_length_);
-  for (int i = 0; i < middle_units_length_; ++i) {
-    weights_[0][i].resize(input_units_length_);
-  }
-  weights_[1].resize(1);
-  weights_[1][0].resize(middle_units_length_);
+  new(&weights_[0]) Eigen::MatrixXd(middle_units_length_, input_units_length_);
+  new(&weights_[1]) Eigen::MatrixXd(1, middle_units_length_);
 
   // 重みの初期化
   std::mt19937 mt((unsigned)time(NULL));
-  for (int k = 0; k < weights_.size(); ++k) {
-    for (int j = 0; j < weights_[k].size(); ++j) {
-      for (int i = 0; i < weights_[k][j].size(); ++i) {
-        weights_[k][j][i] = (double)mt() / (((long long)1<<32)-1) * 2/weights_[k][j].size()
-          - 1.0/weights_[k][j].size();
+  for (int k = 0; k < 2; ++k) {
+    for (int j = 0; j < weights_[k].rows(); ++j) {
+      for (int i = 0; i < weights_[k].cols(); ++i) {
+        weights_[k](j, i) = (double)mt() / (((long long)1<<32)-1) * 2/weights_[k].cols()
+          - 1.0/weights_[k].cols();
       }
     }
   }
@@ -74,6 +72,17 @@ double NeuralNetwork<N>::activation_function(double x)
 }
 
 template<std::size_t N>
+Eigen::MatrixXd NeuralNetwork<N>::activation_function(Eigen::MatrixXd x)
+{
+  for (int j = 0; j < x.rows(); ++j) {
+    for (int i = 0; i < x.cols(); ++i) {
+      x(j, i) = activation_function(x(j, i));
+    }
+  }
+  return x;
+}
+
+template<std::size_t N>
 double NeuralNetwork<N>::derivative_activation_function(double x)
 {
   return (x<0) ? 0 : 1;
@@ -81,94 +90,61 @@ double NeuralNetwork<N>::derivative_activation_function(double x)
 }
 
 template<std::size_t N>
+Eigen::MatrixXd NeuralNetwork<N>::derivative_activation_function(Eigen::MatrixXd x)
+{
+  for (int j = 0; j < x.rows(); ++j) {
+    for (int i = 0; i < x.cols(); ++i) {
+      x(j, i) = derivative_activation_function(x(j, i));
+    }
+  }
+  return x;
+}
+
+template<std::size_t N>
 void NeuralNetwork<N>::fit(double obj_value, std::array<double, 2> exp_values)
 {
-  static int max_unit_length
-    = (input_units_length_ >= middle_units_length_) ?
-    input_units_length_ : middle_units_length_;
+  std::array<Eigen::MatrixXd, 3> inputs = {
+                                           Eigen::MatrixXd(input_units_length_, 1),
+                                           Eigen::MatrixXd(middle_units_length_, 1),
+                                           Eigen::MatrixXd(1, 1),
+  };
 
-  // 慣性項
-  static double inertia_term[2][N][N] = {{}};
-
-  // 入力層以外のニューロンに入る値の総和
-  double neuron[2][max_unit_length] = {{}};
-
-  // 各ニューロンの重みを計算
-  for (int k = 0; k < weights_.size(); ++k) {
-    for (int j = 0; j < weights_[k].size(); ++j) {
-      neuron[k][j] = 0;
-      for (int i = 0; i < weights_[k][j].size(); ++i) {
-        if (k == 0) {
-          neuron[k][j] += weights_[k][j][i] * ((i<exp_values.size()) ? exp_values[i] : 1);
-        } else {
-          neuron[k][j] += weights_[k][j][i] * activation_function(neuron[k-1][i]);
-        }
-      }
-    }
+  // 各ニューロンへの入力和を計算
+  for (int i = 0; i <= exp_values.size(); ++i) {
+    inputs[0](i, 0) = (i<exp_values.size()) ? exp_values[i] : 1;
   }
+  inputs[1] = weights_[0] * inputs[0];
+  inputs[2] = weights_[1] * activation_function(inputs[1]);
 
-  // バックプロパゲーション法による重みの更新
-  for (int j = 0; j < weights_[0].size(); ++j) {
-    for (int i = 0; i < weights_[0][j].size(); ++i) {
-      inertia_term[0][j][i]
-        = learning_rate_
-        * weights_[1][0][i]
-        * (obj_value - activation_function(neuron[1][0]))
-        * derivative_activation_function(neuron[1][0])
-        * derivative_activation_function(neuron[0][i])
-        * ((i<exp_values.size()) ? exp_values[i] : 1)
-        + inertia_weight * inertia_term[0][j][i];
-      weights_[0][j][i] += inertia_term[0][j][i];
-    }
-  }
-  for (int i = 0; i < weights_[1][0].size(); ++i) {
-    inertia_term[1][0][i]
-      = learning_rate_
-      * (obj_value - activation_function(neuron[1][0]))
-      * derivative_activation_function(neuron[1][0])
-      * neuron[0][i]
-      + inertia_weight * inertia_term[1][0][i];
-    weights_[1][0][i] += inertia_term[1][0][i];
-  }
+  // 重みの更新
+  Eigen::MatrixXd teacher(1, 1);
+  teacher(0, 0) = obj_value;
+  std::array<Eigen::MatrixXd, 2> delta;
+  delta[1] = (teacher - activation_function(inputs[2]));
+  delta[0] = (weights_[1].transpose() * delta[1]).array()
+    * derivative_activation_function(inputs[1]).array();
+  weights_[1] += learning_rate_ * delta[1] * activation_function(inputs[1]).transpose();
+  weights_[0] += learning_rate_ * delta[0] * inputs[0].transpose();
 }
 
 template<std::size_t N>
 double NeuralNetwork<N>::predict(std::array<double, 2> exp_values)
 {
-  static int max_unit_length
-    = (input_units_length_ >= middle_units_length_) ?
-    input_units_length_ : middle_units_length_;
-  double a[2][max_unit_length] = {{}};
-  double *pre = a[0], *post = a[1];
-
-  // 出力の計算
-  for (int i = 0; i < weights_[0].size(); ++i) {
-    pre[i] = (i<exp_values.size()) ? exp_values[i] : 1;
+  Eigen::MatrixXd input(exp_values.size() + 1, 1);
+  for (int i = 0; i <= exp_values.size(); ++i) {
+    input(i, 0) = (i<exp_values.size()) ? exp_values[i] : 1;
   }
-  for (int k = 0; k < weights_.size(); ++k) {
-    for (int j = 0; j < weights_[k].size(); ++j) {
-      post[j] = 0;
-      for (int i = 0; i < weights_[k][j].size(); ++i) {
-        post[j] += weights_[k][j][i] * pre[i];
-      }
-      post[j] = activation_function(post[j]);
-    }
-    double *t = pre;
-    pre = post;
-    post = t;
-  }
-
-  return pre[0];
+  return (weights_[1] * activation_function(weights_[0] * input))(0, 0);
 }
 
 template<std::size_t N>
 void NeuralNetwork<N>::show_weights()
 {
-  for (int k = 0; k < weights_.size(); ++k) {
+  for (int k = 0; k < 2; ++k) {
     printf("weights between layer %d - %d:\n", k-1, k);
-    for (int j = 0; j < weights_[k].size(); ++j) {
-      for (int i = 0; i < weights_[k][j].size(); ++i) {
-        printf("%s%6.3f", (i==0)?"":" ", weights_[k][j][i]);
+    for (int j = 0; j < weights_[k].rows(); ++j) {
+      for (int i = 0; i < weights_[k].cols(); ++i) {
+        printf("%s%7.3f", (i==0)?"":" ", weights_[k](j, i));
       }
       puts("");
     }
